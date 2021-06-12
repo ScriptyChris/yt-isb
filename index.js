ac9565fb6083e1d9be9fd0e7d535eecc8cd52962
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-06-12 23:33:42 +0200

    Launch YouTube page on separate browser profile, configured from env;

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer-core');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, executablePath: process.env.CHROMIUM_BROWSER_PATH });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    executablePath: process.env.CHROMIUM_BROWSER_PATH,
+    args: [`--user-data-dir=${process.env.BROWSER_PROFILE_PATH}`, '--no-first-run', '--no-default-browser-check'],
+  });
+  const [page] = await browser.pages();
   await page.goto('https://youtube.com');
 
-  setTimeout(async () => await browser.close(), 3000);
+  // setTimeout(async () => await browser.close(), 3000);
 })();
